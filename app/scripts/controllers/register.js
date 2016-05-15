eea2892a041202f24566ab0a6963ba79cf9f552e
Maria <mmorrison1978@gmail.com>
2016-05-14 23:31:11 -0600

    updated

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -6,5 +6,11 @@ angular.module('jwtAngularApp').controller('RegisterCtrl', function ($scope) {
 			email: $scope.email,
 			password: $scope.password
 		})
+			.then(function (res) {
+				alert('success', 'Account Created!', 'Welcome, ' + res.data.user.email + '! Please email activate your account in the next several days.');
+			})
+			.catch(function (err) {
+				alert('warning', 'Unable to create account :(', err.message);
+			});
 	};
 });
--- app/scripts/services/alert.js
+++ app/scripts/services/alert.js
@@ -1,13 +1,31 @@
 'use strict';
 
-/**
- * @ngdoc service
- * @name jwtAngularApp.alert
- * @description
- * # alert
- * Service in the jwtAngularApp.
- */
-angular.module('jwtAngularApp')
-  .service('alert', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-  });
+angular.module('jwtAngularApp').service('alert', function alert($rootScope, $timeout) {
+    var alertTimeout;
+
+    //    $rootScope.$on('$stateChangeStart',
+    //        function(event, toState, toParams, fromState, fromParams) {
+    //            $rootScope.alert.hide = true;
+    //            $rootScope.alert.show = false;
+    //            console.log($rootScope.alert.show);
+    //        })
+    //    $rootScope.$on('$stateChangeSuccess',
+    //        function(event, toState, toParams, fromState, fromParams) {
+    //            $timeout.cancel(alertTimeout);
+    //            //$rootScope.alert.hide = false;
+    //        })
+
+    return function(type, title, message, timeout) {
+        $rootScope.alert = {
+            hasBeenShown: true,
+            show: true,
+            type: type,
+            message: message,
+            title: title
+        };
+        $timeout.cancel(alertTimeout);
+        alertTimeout = $timeout(function() {
+            $rootScope.alert.show = false;
+        }, timeout || 2000);
+    }
+});
