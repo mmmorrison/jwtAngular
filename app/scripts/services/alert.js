685c5e589cb1f0476b80dfab6508f2a88e035b49
Maria <mmorrison1978@gmail.com>
2016-05-14 22:13:53 -0600

    added service

---
+++ app/scripts/services/alert.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name jwtAngularApp.alert
+ * @description
+ * # alert
+ * Service in the jwtAngularApp.
+ */
+angular.module('jwtAngularApp')
+  .service('alert', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
